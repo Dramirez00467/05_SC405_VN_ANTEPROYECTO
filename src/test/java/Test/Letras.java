2a63c6186d4e7634ac510b7e8d3f5aeb9fa018be
Dramirez00467 <106043587+Dramirez00467@users.noreply.github.com>
2022-08-09 23:03:19 -0600

    Update Letras.java

--- src/test/java/Test/Letras.java
+++ src/test/java/Test/Letras.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Letras{
 	
 @Test
-public static  void  main(String[] args) { 
+public void  main() { 
 	  System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver1/chromedriver.exe");
 	  ChromeDriver driver = new ChromeDriver(); 
 	  driver.navigate().to("https://demoqa.com/webtables");
